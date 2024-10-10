c467d8f72fc74cf3d38afda2a8f7fe45ae951700
Ye Lwin Oo <yelwinoo.work@gmail.com>
2024-10-10 15:25:28 +0700

    updated: effect dto value removed

--- lib/src/dto/effect_dto.dart
+++ lib/src/dto/effect_dto.dart
@@ -3,8 +3,6 @@ part of 'dto.dart';
 class EffectDto {
   final int index;
   final String text;
-  double get value => 1.0;
-  double get invValue => 1.0 - value;
 
   EffectDto({
     required this.index,
